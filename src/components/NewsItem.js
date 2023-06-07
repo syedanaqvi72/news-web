7281e144ec9d88717f3d17424be989b8bbd1d3a2
Mehak <mehakzehra5@gmail.com>
2023-06-07 11:39:39 -0700

    final touch

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,4 +1,3 @@
-
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
@@ -6,28 +5,28 @@ export class NewsItem extends Component {
   let  {title,description, imageUrl, url,author, date }= this.props;
 
   return (
-    <div className="card mb-3">
+      <div className="my-3">
                <div className="card" style={{width: "18rem"}}>
+  <div className="card-body">
   <img src={!imageUrl?"https://ichef.bbci.co.uk/news/1024/branded_news/779B/production/_129991603_mediaitem129991112.jpg":imageUrl} className="card-img-top" alt="..."/>
   <div className="card-body">
+  <br></br>
+<a className="btn btn-primary" href="https://facebook.com/dawn.com" role="button">Facebook</a>
+<a className="btn btn-primary" href="https://twitter.com/dawn.com" role="button">twitter</a>
+<a className="btn btn-primary" href="https://instagram.com/dawndot.com" role="button">Instagram</a>
     <h5 className="card-title">{title}...</h5>
     <p className="card-text">{description}...</p>
-    <div class="card-footer">
-      <p className="card-text">
-      <a href="https://facebook.com/dawn.com" className="card-link">Facebook</a>
-    <a href="https://twitter.com/dawn.com" className="card-link">Twitter</a>
-    <a href="https://instagram.com/dawndot.com" className="card-link">Instagram</a>
-      <small className="text-muted">By  : {!author?"Unknow": author} <br></br>on : <u>{date}</u></small></p>
-    </div>
-</div>
 
-    <a href={url} target="_blank" className="btn btn-sm btn-primary" rel="noreferrer">Read More </a><br></br>
-    <br></br>
-    
-   
+    <a href={url} target="_blank" className="btn btn-sm btn-primary" rel="noreferrer">Read More </a>
+    <div className="card-footer">
+      <p className="card-text">
+      <small className="text-muted">By  :<b> {!author?"Unknow": author}</b> <br></br>on : <u>{date}</u></small></p>
+    </div>
    
   </div>
 </div>
+</div>
+      </div>
       
     );
   }
