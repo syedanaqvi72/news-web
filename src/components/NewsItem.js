8012503368b13778dcd6b40c1ddd90d3713e504a
Mehak <mehakzehra5@gmail.com>
2023-06-07 11:11:13 -0700

    finally

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,8 +1,9 @@
+
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-  let  {title,description, imageUrl, url, }= this.props;
+  let  {title,description, imageUrl, url,author, date }= this.props;
 
   return (
       <div className="my-3">
@@ -12,12 +13,18 @@ export class NewsItem extends Component {
   <div className="card-body">
     <h5 className="card-title">{title}...</h5>
     <p className="card-text">{description}...</p>
+    <div class="card-footer">
+      <p className="card-text">
+      <small className="text-muted">By  : {!author?"Unknow": author} <br></br>on : <u>{date}</u></small></p>
+    </div>
+
+    <a href={url} target="_blank" className="btn btn-sm btn-primary" rel="noreferrer">Read More </a><br></br>
+    <br></br>
+    <a href="https://facebook.com/dawn.com" className="card-link">Facebook</a>
+    <a href="https://twitter.com/dawn.com" className="card-link">Twitter</a>
+    <a href="https://instagram.com/dawndot.com" className="card-link">Instagram</a>
    
-    <a href={url} target="_blank" className="btn btn-sm btn-primary" rel="noreferrer">Read More
-    </a>
-    <a href="/" className="card-link">facebook</a>
-    <a href="/" className="card-link">instagram</a>
-    <a href="/" className="card-link">twitter</a>
+   
   </div>
 </div>
 </div>
